26a95caf25f5b3f9dee5c6c01256919180a1170d
Ibrahim Ahmed <13389823+ibz096@users.noreply.github.com>
2022-11-27 17:53:36 -0500

    Update index.js
    Copied code from my Replit

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv').config()
+const mySecret = process.env['MONGO_URI']
+
+const mongoose = require('mongoose');
+
+mongoose.connect(mySecret,{ useNewUrlParser: true, useUnifiedTopology: true })
+  .then(
+    () => { console.log("Connected") },
+    err => { console.log(`Database Connection: Failure.\n${err}`) }
+  );
+//New User Schema and Model
+const userSchema = new mongoose.Schema({
+  username: String
+});
+
+const User = mongoose.model('User', userSchema);
+//test
+// New Execercise Schema and Model
 
 app.use(cors())
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
+app.post('/api/users', (req, res) => {
+  //Save New User
+  let newUser = new User({username: req.body.username})
+  newUser.save((err, data) => {
+    if (err) return err
+    res.send(data)
+  }) 
+  
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {

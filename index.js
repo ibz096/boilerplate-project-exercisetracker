ed42b4c5847c4be05084a69f72decbbcff519899
Ibrahim Ahmed <13389823+ibz096@users.noreply.github.com>
2022-12-02 03:34:02 +0000

    Added a /api/users/:_id/logs endpoint

--- index.js
+++ index.js
@@ -9,17 +9,26 @@ const mongoose = require('mongoose');
 
 mongoose.connect(mySecret,{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(
-    () => { console.log("Connected") },
+    () => { console.log("DB Connected") },
     err => { console.log(`Database Connection: Failure.\n${err}`) }
   );
-//New User Schema and Model
+//New User Schema
 const userSchema = new mongoose.Schema({
-  username: String
+  username: String,
 });
 
+//Exercise Schema
+const exerciseSchema = new mongoose.Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date,
+  user_id: String
+});
+//User Model
 const User = mongoose.model('User', userSchema);
-//test
-// New Execercise Schema and Model
+// Exercise Model
+const Exercise = mongoose.model('Exercise', exerciseSchema)
 
 app.use(cors())
 app.use(express.static('public'))
@@ -31,6 +40,12 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, data) => {
+    res.send(data)
+  })
+})
 
 app.post('/api/users', (req, res) => {
   //Save New User
@@ -45,11 +60,72 @@ app.post('/api/users', (req, res) => {
   
 })
 
-app.get('/api/users', (req, res) => {
-  User.find({}, (err, data) => {
-    console.log(data)
-    res.send(data)
-  })
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  let userId = req.params._id
+  //Check if date field is empty. If no date is supplied, the current date will be used.
+  let date = ( req.body.date ? req.body.date : new Date())
+  
+  try {
+    //Check if user exists in DB
+    const user = await User.findById(userId).exec();
+    //If user exists create new Excercise record
+    if (user) {
+      let newExercise = new Exercise({
+        username: user.username,
+        description: req.body.description,
+        duration: req.body.duration,
+        date: date,
+        user_id: user._id
+      })
+      console.log(newExercise)
+      try {
+        //Save record
+        let saveExercise = await newExercise.save()
+        // Send JSON response of the newly created object
+        res.json({
+          username: saveExercise.username,
+          description: saveExercise.description,
+          duration: saveExercise.duration,
+          date: new Date(saveExercise.date).toDateString(),
+          _id: saveExercise.user_id
+        })
+        // console.log()
+      } catch (error) {
+        console.log(`Error while saving Exercise document: ${error}`)
+      }
+      
+    } else { res.send('User not found') }
+    
+  } catch (error) {
+    console.log(error)
+    res.send('Error. Please view the console')
+  }
+  
+})
+
+//Make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
+app.get('/api/users/:_id/logs', async (req, res) => {
+  let userId = req.params._id;
+  //Query the Exercise collection where using the userId(_id) provided
+  try {
+    let exercises = await Exercise.find({user_id: userId}).exec();
+    console.log(exercises)
+    //count = exercises.length
+    res.json({
+      username: exercises[0].username,
+      count: exercises.length,
+      _id: exercises[0].user_id,
+      log: exercises.map( (obj) => ({
+        description: obj.description,
+        duration: obj.duration,
+        date: obj.date
+        
+  }))
+    });
+  } catch (error) {
+    console.log(error)
+    res.send('Error. Please view the console')
+  }
 })
 
 
